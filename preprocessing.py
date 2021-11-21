36e4d6884715ee037f172f68355f9fab19f323ce
Rovlet <zwolinskajustyna12@gmail.com>
2021-11-21 15:54:17 +0100

    Add multiprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -5,6 +5,11 @@ import pandas as pd
 import seaborn as sn
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
+import multiprocessing
+import json
+from PIL import Image as im
+
+from sklearn.model_selection import train_test_split
 
 from encoder import Encoder
 from settings import *
@@ -25,22 +30,34 @@ def prepare_data(df):
 
 
 def prepare_session_data(df):
+    encoder = Encoder(df)
+    if df.empty:
+        return
     start_time = df[' Timestamp'].iloc[0]
     df.loc[df[' Timestamp'] != start_time, ' Timestamp'] = 0
     df.loc[df[' Timestamp'] == start_time, ' Timestamp'] = 1
-    return df
+    label = df[' Label'].iloc[0]
+    encoded_session = encoder.start_encoding_process(df)
+    return df, encoded_session, label
 
 
-def drop_highly_correlated_columns(df):
+def get_highly_correlated_columns(df):
     corrMatrix = df.corr().abs()
     upper = corrMatrix.where(np.triu(np.ones(corrMatrix.shape), k=1).astype(bool))
     to_drop = [column for column in upper.columns if any(upper[column] > 0.95)]
-    df.drop(to_drop, axis=1, inplace=True)
 
     if DISPLAY_CORR_MATRICES:
         sn.heatmap(corrMatrix)
         plt.show()
-    return df
+        corrMatrix = df.corr().abs()
+        sn.heatmap(corrMatrix)
+        plt.show()
+    return to_drop
+
+
+def get_rows_with_the_same_id(arguments):
+    df, session_id = arguments
+    return df.loc[df['Flow ID'] == session_id]
 
 
 if __name__ == '__main__':
@@ -48,23 +65,52 @@ if __name__ == '__main__':
     files = [filename for filename in directory if filename.endswith('csv')]
     frames = []
     for file in files:
-        df = pd.read_csv(DATA_PATH + file)
-        encoder = Encoder(df)
-        data_file = file.strip('.csv')
-        folder = os.path.join(f'./data/{data_file}')
-        if not os.path.isdir(folder):
-            os.makedirs(folder)
+        df = pd.read_csv(DATA_PATH + file, encoding='utf-8',
+                         dtype={'Flow ID': np.unicode_, ' Source IP': np.unicode_, " Destination IP": np.unicode_,
+                                " Timestamp": np.unicode_, " Label": np.unicode_})
+        frames.append(df)
+    df = pd.concat(frames)
 
-        df = prepare_data(df)
-        df = drop_highly_correlated_columns(df)
-        all_unique_sessions = list(df['Flow ID'].unique())
+    df = prepare_data(df)
+    to_drop = get_highly_correlated_columns(df)
 
-        for i, session in enumerate(all_unique_sessions):
-            session_df = df.loc[df['Flow ID'] == session]
-            if len(session_df) >= MINIMUM_LOGS_FROM_SESSION:
-                session_df = prepare_session_data(session_df)
-                frames.append(session_df)
-                encoder.start_encoding_process(df, path=f'./data/{data_file}/{i}.png')
+    df.drop(to_drop, axis=1, inplace=True)
 
-    result_df = pd.concat(frames)
-    result_df.to_csv(f"results.csv")
+    all_unique_sessions = list(df['Flow ID'].unique())
+
+    num_workers = multiprocessing.cpu_count() - 3
+    print(f"Numbers of workers: {num_workers}")
+    arguments = [[df, session_id] for session_id in all_unique_sessions]
+
+    with multiprocessing.Pool(num_workers) as pool:
+        frames = pool.map(get_rows_with_the_same_id, arguments)
+    arguments = frames
+
+
+    X = []
+    y = []
+
+    with multiprocessing.Pool(num_workers) as pool:
+        frames = pool.map(prepare_session_data, arguments)
+
+    all_df = []
+    for i, frame in enumerate(frames):
+        if frame is not None:
+            df, encoded_session, label = frame
+            if encoded_session is not None:
+                all_df.append(df)
+                X.append(encoded_session)
+                y.append(label)
+                if SAVE_ENCODED_PICTURES:
+                    data = im.fromarray(encoded_session)
+                    Encoder.save_encoded_picture(data, path=f'./pictures/{i}.png')
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42, stratify=y)
+
+    np.save('X_train.npy', X_train, allow_pickle=True)
+    np.save('X_test.npy', X_test, allow_pickle=True)
+    np.save('y_train.npy', y_train, allow_pickle=True)
+    np.save('y_test.npy', y_test, allow_pickle=True)
+
+    all_df = pd.concat(all_df)
+    all_df.to_csv(f'results.csv', header=True, index=False)
